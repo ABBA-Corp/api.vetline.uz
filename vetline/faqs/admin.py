90195ea297e000ee66cebf1c84bfea847266fb37
Kairat Sultan <garri.sultonov@mail.ru>
2023-02-23 16:01:09 +0600

    faqs ordering

--- vetline/faqs/admin.py
+++ vetline/faqs/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from ordered_model.admin import OrderedModelAdmin
+
 from .models import Faqs
 
 
 @admin.register(Faqs)
-class FaqsAdmin(admin.ModelAdmin):
-    list_display = ['faq_uz', 'answer_uz']
+class FaqsAdmin(OrderedModelAdmin):
+    list_display = ['faq_uz', 'answer_uz', 'move_up_down_links']
--- vetline/faqs/models.py
+++ vetline/faqs/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from ordered_model.models import OrderedModel
 
-class Faqs(models.Model):
-    faq_uz = models.CharField(_("Вопрос uz"), max_length=255)
-    faq_ru = models.CharField(_("Вопрос ru"), max_length=255, null=True, blank=True)
-    faq_en = models.CharField(_("Вопрос en"), max_length=255, null=True, blank=True)
-    answer_uz = models.CharField(_("Ответ uz"), max_length=255)
-    answer_ru = models.CharField(_("Ответ ru"), max_length=255, null=True, blank=True)
-    answer_en = models.CharField(_("Ответ en"), max_length=255, null=True, blank=True)
+
+class Faqs(OrderedModel):
+    faq_uz = models.CharField(_("Вопрос uz"), max_length=500)
+    faq_ru = models.CharField(_("Вопрос ru"), max_length=500, null=True, blank=True)
+    faq_en = models.CharField(_("Вопрос en"), max_length=500, null=True, blank=True)
+    answer_uz = models.CharField(_("Ответ uz"), max_length=500)
+    answer_ru = models.CharField(_("Ответ ru"), max_length=500, null=True, blank=True)
+    answer_en = models.CharField(_("Ответ en"), max_length=500, null=True, blank=True)
 
     class Meta:
         verbose_name = _("Вопрос")
